05479f6cf3dfdb0bfe3b909b6fc9c8c898a0819c
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-07-01 18:51:29 +0500

    initial scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TicTacToe from '@/pages/TicTacToe';
+
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <div className="text-pink-600 text-xl">Loading Tic Tac Toe...</div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TicTacToe />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
